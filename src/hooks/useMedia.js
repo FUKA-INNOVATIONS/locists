7c0cd41a7c0c75119e0a7fc7bcec3a7828b52b99
David Fallow <davidfa@metropolia.fi>
2022-02-16 15:06:44 +0200

    useMedia and Tag files started

---
+++ src/hooks/useMedia.js
@@ -0,0 +1,19 @@
+import {useState} from "react";
+import axios from "axios";
+
+// TODO: use appId
+import { baseUrl } from "../../config";
+
+
+const useMedia = () => {
+    const [ loading, setLoading] = useState(false);
+
+    const getAllMedia = async () => {
+        setLoading(true);
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
