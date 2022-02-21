f1e1783884755faa5f0182091981fb894a94e608
fuka-innovations <fuad.kalhori@gmail.com>
2022-02-21 15:00:37 +0200

    fix linting

--- src/views/Account.js
+++ src/views/Account.js
@@ -1,4 +1,4 @@
-import { useCallback, useLayoutEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { Button, Text, View, Image } from 'react-native';
 import useAuthStorage from '../hooks/useAuthStorage';
 import UploadMedia from '../components/UploadMedia';
