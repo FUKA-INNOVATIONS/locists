fe9059c3af5841292a7156db709c7076e50f2edc
fuka-innovations <fuad.kalhori@gmail.com>
2022-02-27 13:41:27 +0200

    comment added time ago

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,4 +1,5 @@
 import { View, Text, Image } from 'react-native';
+import TimeAgo from '@andordavoti/react-native-timeago';
 
 const Comment = ( { commentObj, avatar } ) => {
   // const { comment_id, file_id, user_id, comment, time_added } = commentObj;
@@ -7,7 +8,7 @@ const Comment = ( { commentObj, avatar } ) => {
       <View>
         <Image source={{uri: 'http://placekitten.com/35/35'}} style={ { width: 35, height: 35 } } />
         <Text>Comment: {commentObj.comment}</Text>
-        <Text>Added: {commentObj.time_added}</Text>
+        <Text>Commented <TimeAgo dateTo={new Date(commentObj.time_added)} /></Text>
         <Text>Likes</Text>
       </View>
   );
