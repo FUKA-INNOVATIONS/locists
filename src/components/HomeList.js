886af54deabeb6b701e0ce7e6a1c61692d0b6727
fuka-innovations <fuad.kalhori@gmail.com>
2022-03-01 17:27:28 +0200

    mid commit

--- src/components/HomeList.js
+++ src/components/HomeList.js
@@ -1,22 +1,26 @@
-import { FlatList, Pressable, View, Text } from 'react-native';
-import Post from './Post';
-import Event from './Event';
-import useMedia from '../hooks/useMedia';
-import { useEffect, useMemo, useState } from 'react';
-import { useIsFocused } from '@react-navigation/native';
+import { FlatList, Pressable, View, Text } from 'react-native'
+import Post from './Post'
+import Event from './Event'
+import useMedia from '../hooks/useMedia'
+import { useEffect, useMemo, useState } from 'react'
+import { useIsFocused } from '@react-navigation/native'
+import sortLatest from '../utils/sortLatest'
 
 const HomeList = ( { navigation } ) => {
-  const { getAllMedia, allMedia } = useMedia();
+  const { getAllMedia, allMedia } = useMedia()
   // TODO: fix with focus listener
-  const viewIsFocused = useIsFocused(); // eslint-disable-line
-  const [ loading, setLoading ] = useState( false );
+  const viewIsFocused = useIsFocused() // eslint-disable-line
+  const [ loading, setLoading ] = useState( false )
 
-   const getPostsAndEvents = useMemo( async () => {
+  const getPostsAndEvents = useMemo( async () => {
     await getAllMedia()
-  }, [] );
+  }, [] )
 
-   // console.log('HomeList.js')
-   // console.log('HomeList.js => allMedia', allMedia)
+
+
+
+  // console.log('HomeList.js')
+  console.log('HomeList.js => allMedia', allMedia)
 
   // TODO: fix rendering
 
@@ -24,59 +28,58 @@ const HomeList = ( { navigation } ) => {
   // onEndReached={this.onScrollHandler} , onEndThreshold={0}
   // No good solutions with the api available, considering the way we use the api
 
-   useEffect( async () => {
-    setLoading( true );
+  useEffect( async () => {
+    setLoading( true )
     await getPostsAndEvents
     // await getAllMedia()
-    setLoading( false );
-  }, [] );
-
+    setLoading( false )
+  }, [] )
 
   if ( loading ) {  // TODO: Add a spinner icon
     return (
-        <View>
-          <Text>
-            Loading..
-          </Text>
-        </View>
-    );
+      <View>
+        <Text>
+          Loading..
+        </Text>
+      </View>
+    )
   }
 
   // Move user to single event view when tapping event card
   const eventPressHandler = ( eventId ) => {
-    navigation.navigate( 'SingleEvent', { eventId: eventId } );
-  };
+    navigation.navigate( 'SingleEvent', { eventId: eventId } )
+  }
 
   const postPressHandler = ( postId ) => {
-    navigation.navigate( 'SinglePostHomeStack', { postId: postId } );
-  };
+    navigation.navigate( 'SinglePostHomeStack', { postId: postId } )
+  }
 
-  const EmptyListMessage = () => <Text>No events </Text>;
+  const EmptyListMessage = () => <Text>No events </Text>
 
   // console.log('all media', allMedia)
 
   return (
-      <FlatList
-          data={ allMedia }
-          ListEmptyComponent={ EmptyListMessage }
-          keyExtractor={ ( item ) => item.file_id }
-          renderItem={ ( { item } ) => {
-            return (
-                item.description.mediaType === 'post' ?
-                    <Pressable
-                        onPress={ () => postPressHandler( item.file_id ) }>
-                      <Post postMedia={ item }/>
-                    </Pressable>
-                    :
-                    <Pressable
-                        onPress={ () => eventPressHandler( item.file_id ) }>
-                      <Event eventDetails={ item }/>
-                    </Pressable>
-            );
-          }
-          }
-      />
-  );
-};
+    <FlatList
+      data={ allMedia }
+      ListEmptyComponent={ EmptyListMessage }
+      keyExtractor={ ( item ) => item.file_id }
+      renderItem={ ( { item } ) => {
+        return (
+          item.description.mediaType === 'post' ?
+            <Pressable
+              onPress={ () => postPressHandler( item.file_id ) }>
+              <Post postMedia={ item } />
+            </Pressable>
+            :
+            <Pressable
+              onPress={ () => eventPressHandler( item.file_id ) }>
+              <Event eventDetails={ item } />
+            </Pressable>
+        )
+      }
+      }
+    />
+  )
+}
 
-export default HomeList;
+export default HomeList
