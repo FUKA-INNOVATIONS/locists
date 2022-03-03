da70680a09f10c9821b31d0ca0c488b6f81d58e1
fuka-innovations <fuad.kalhori@gmail.com>
2022-03-03 23:22:06 +0200

    fix linting

--- src/utils/sortFilterHelpers.js
+++ src/utils/sortFilterHelpers.js
@@ -1,5 +1,3 @@
-import { Image } from 'react-native';
-
 export const sortLatest = (arr) => {
   return arr.sort(function(a,b){
     return Number( new Date( b.time_added ) ) - Number( new Date( a.time_added ) );
