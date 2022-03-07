adbb9b101466b23c92775230dd8597a2288fab24
fuka-innovations <fuad.kalhori@gmail.com>
2022-03-07 08:46:38 +0200

    removed goBach button on create event and post

--- src/views/createPost.js
+++ src/views/createPost.js
@@ -1,14 +1,9 @@
 import UploadMedia from '../components/UploadMedia'
-import { Button } from 'react-native'
 
 const CreatePost = ( { navigation } ) => {
-  const onModalCloseHandler = () => {
-    navigation.goBack()
-  }
 
   return (
     <>
-      <Button title={ 'Close' } onPress={ onModalCloseHandler } />
       <UploadMedia mediaType={ 'post' } navigation={ navigation } />
     </>
   )
