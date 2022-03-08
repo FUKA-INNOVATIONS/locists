35fb072cd717e679ea6a6c3ef2649ee88c0b41ba
David Fallow <davidfa@metropolia.fi>
2022-03-08 12:29:32 +0200

    splashScreen added

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import React from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import { StatusBar } from 'expo-status-bar'
 import AppNavigator from './src/navigator/AppNavigator'
 
 import AuthStorage from './src/utils/authStorage'
 import AuthStorageContext from './src/context/AuthStorageContext'
 import theme from './src/theme'
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, View, Dimensions, StyleSheet} from 'react-native'
+import * as SplashScreen from 'expo-splash-screen';
+import { SplashScreenLogo, SplashBack } from './src/utils'
 
 // Create new instance of authentication storage
 const authStorage = new AuthStorage()
+const dimensions = Dimensions.get( 'screen' )
 
 export default function App() {
+
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect( () => {
+    async function prepare() {
+      try {
+        await new Promise(resolve => setTimeout(resolve, 4000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, [] );
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return (
+      <View style={ styles.splashScreen}>
+        <SplashScreenLogo width={dimensions.width * 0.7} height={dimensions.height * 0.8} />
+      </View>
+    )
+  }
+
   console.log( 'App.js' )
   return (
     <>
       <AuthStorageContext.Provider value={ authStorage }>
-        <SafeAreaView style={ theme.AndroidSafeArea }>
+        <SafeAreaView style={ theme.AndroidSafeArea }
+                      onLayout={ onLayoutRootView }>
           <AppNavigator />
         </SafeAreaView>
       </AuthStorageContext.Provider>
@@ -23,3 +57,18 @@ export default function App() {
     </>
   )
 }
+
+const styles = StyleSheet.create({
+  splashScreen: {
+    width: dimensions.width,
+    height: dimensions.height,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  splashBack: {
+    width: dimensions.width,
+    height: dimensions.height,
+    position: 'absolute',
+  }
+})
